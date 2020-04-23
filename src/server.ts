ba1b24292358f1c3d8c20f307ea01e2fc2d246e0
Lucas <lucas.raziel@gmail.com>
2020-04-23 19:54:11 -0300

    template with test and database

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,6 @@
 /* eslint-disable no-console */
-import 'reflect-metadata';
-
-import express from 'express';
-
-import routes from './routes';
-
-const app = express();
-
-app.use(express.json());
-
-app.use(routes);
+import app from './app';
 
 app.listen(3333, () => {
-  console.log('Server started 🐱‍🏍');
+  console.log('🚀 Server started on port 3333!');
 });
